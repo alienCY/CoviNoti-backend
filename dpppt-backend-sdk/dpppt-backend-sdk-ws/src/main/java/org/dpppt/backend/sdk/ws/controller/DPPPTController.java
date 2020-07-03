388ff7b5cf7d91bcba9605509801d1374007a7fd
cpepi001 <cpepi001@outlook.com>
2020-07-03 15:46:18 +0300

    Countries of Interest

--- dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/DPPPTController.java
+++ dpppt-backend-sdk/dpppt-backend-sdk-ws/src/main/java/org/dpppt/backend/sdk/ws/controller/DPPPTController.java
@@ -195,6 +195,32 @@ public class DPPPTController {
 	}
 
 	@CrossOrigin(origins = { "https://editor.swagger.io" })
+	@GetMapping(value = "/exposed/{batchReleaseTime}/{coi}", produces = "application/x-protobuf")
+	public @ResponseBody ResponseEntity<Exposed.ProtoExposedList> getExposedByBatch(@PathVariable long batchReleaseTime,
+	       @PathVariable String coi, WebRequest request) throws BadBatchReleaseTimeException {
+		if(!validationUtils.isValidBatchReleaseTime(batchReleaseTime)) {
+			return ResponseEntity.notFound().build();
+		}
+
+		//TODO: Filter out
+		System.out.println(coi);
+
+		List<Exposee> exposeeList = dataService.getSortedExposedForBatchReleaseTime(batchReleaseTime, batchLength);
+		List<Exposed.ProtoExposee> exposees = new ArrayList<>();
+		for (Exposee exposee : exposeeList) {
+			Exposed.ProtoExposee protoExposee = Exposed.ProtoExposee.newBuilder()
+					.setKey(ByteString.copyFrom(Base64.getDecoder().decode(exposee.getKey())))
+					.setKeyDate(exposee.getKeyDate()).build();
+			exposees.add(protoExposee);
+		}
+		Exposed.ProtoExposedList protoExposee = Exposed.ProtoExposedList.newBuilder().addAllExposed(exposees)
+				.setBatchReleaseTime(batchReleaseTime).build();
+
+		return ResponseEntity.ok().cacheControl(CacheControl.maxAge(Duration.ofMinutes(exposedListCacheControl)))
+				.header("X-BATCH-RELEASE-TIME", Long.toString(batchReleaseTime)).body(protoExposee);
+	}
+
+	@CrossOrigin(origins = { "https://editor.swagger.io" })
 	@GetMapping(value = "/buckets/{dayDateStr}", produces = "application/json")
 	public @ResponseBody ResponseEntity<BucketList> getListOfBuckets(@PathVariable String dayDateStr) {
 		OffsetDateTime day = LocalDate.parse(dayDateStr).atStartOfDay().atOffset(ZoneOffset.UTC);
